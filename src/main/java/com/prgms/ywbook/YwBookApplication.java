3aac7447a5085dad22c7328c1915c69f97dd0439
young970 <yyww9@naver.com>
2023-07-21 02:37:40 +0900

    chore: 초기셋팅

---
+++ src/main/java/com/prgms/ywbook/YwBookApplication.java
@@ -0,0 +1,13 @@
+package com.prgms.ywbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YwBookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YwBookApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/prgms/ywbook/YwBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prgms.ywbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YwBookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
